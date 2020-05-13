166a57baa90fc86fba3a51e541dd46c866fda862
margarita <margarita.matulenko@gmail.com>
2020-05-13 21:03:31 +0300

    StackViews add

---
+++ StackView/StackView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackView
+//
+//  Created by Margarita Matulenko on 20/04/2020.
+//  Copyright © 2020 Margarita Matulenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

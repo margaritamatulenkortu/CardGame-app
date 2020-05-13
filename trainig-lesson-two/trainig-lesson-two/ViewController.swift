b0ae8b410509d4c8c4911a215c92161f82f0a0e6
margarita <margarita.matulenko@gmail.com>
2020-05-13 21:01:47 +0300

    two diferent projects edited

---
+++ trainig-lesson-two/trainig-lesson-two/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  trainig-lesson-two
+//
+//  Created by Margarita Matulenko on 15/04/2020.
+//  Copyright © 2020 Margarita Matulenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
